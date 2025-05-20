b436410bd94cc8cdf4fb9813aca5811c5ba1a4b9
Marian13 <mariankostyk13895@gmail.com>
2025-05-21 02:10:46 +0300

    chore(gem): create new gem with "bundle gem semantic_boolean --test=rspec --ci=github --mit --no-coc --changelog --linter=standard --git --github-username=marian13" command

---
+++ lib/semantic_boolean.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "semantic_boolean/version"
+
+module SemanticBoolean
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/semantic_boolean/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SemanticBoolean
+  VERSION = "0.1.0"
+end
