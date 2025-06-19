7c27f17ca1e401be91f5ba428a5ef477856b4883
Marian13 <mariankostyk13895@gmail.com>
2025-06-19 14:44:05 +0300

    chore(coverage): track coverage for TruffleRuby

--- spec/coverage_helper.rb
+++ spec/coverage_helper.rb
@@ -1,25 +1,23 @@
 # frozen_string_literal: true
 
-if !RUBY_ENGINE.match?(/truffleruby/)
-  require "simplecov"
-  require "simplecov-lcov"
+require "simplecov"
+require "simplecov-lcov"
 
-  SimpleCov::Formatter::LcovFormatter.config do |config|
-    config.report_with_single_file = true
+SimpleCov::Formatter::LcovFormatter.config do |config|
+  config.report_with_single_file = true
 
-    config.single_report_path = File.join("coverage", RUBY_ENGINE, RUBY_ENGINE_VERSION, "lcov.info")
+  config.single_report_path = File.join("coverage", RUBY_ENGINE, RUBY_ENGINE_VERSION, "lcov.info")
+end
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::LcovFormatter
+])
+
+SimpleCov.start do
+  if Gem::Version.create(RUBY_VERSION) >= ::Gem::Version.create("2.5")
+    enable_coverage :branch
   end
 
-  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-    SimpleCov::Formatter::HTMLFormatter,
-    SimpleCov::Formatter::LcovFormatter
-  ])
-
-  SimpleCov.start do
-    if Gem::Version.create(RUBY_VERSION) >= ::Gem::Version.create("2.5")
-      enable_coverage :branch
-    end
-
-    add_filter "/spec/"
-  end
+  add_filter "/spec/"
 end
